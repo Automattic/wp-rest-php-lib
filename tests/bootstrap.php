9231cc9f5e620daae638614f411cbcaf3d958d00
Mohammad Jangda <batmoo@gmail.com>
2014-07-17 00:48:16 -0400

    Tests: look for a local config for senstive data
    Like application key, secret, token, etc.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,11 @@
 <?php
 
+$config_file = dirname( __FILE__ ) . '/config.php';
+if ( is_readable( $config_file ) ) {
+	require( $config_file );
+}
+unset( $config_file );
+
 require( dirname( dirname( __FILE__ ) ) . '/class-wpcom-rest-client.php' );
 
 class WPCOM_REST_Transport_Mock extends WPCOM_REST_Transport {
---
+++ tests/config-sample.php
@@ -0,0 +1,5 @@
+<?php
+
+define( 'WPCOM_REST_CLIENT_API_KEY', '' );
+define( 'WPCOM_REST_CLIENT_API_SECRET', '' );
+define( 'WPCOM_REST_CLIENT_API_TOKEN', '' );
