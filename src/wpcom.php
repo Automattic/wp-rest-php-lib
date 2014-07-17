2687a0b9677ec04ed0df5080848383dea2b4def3
Mohammad Jangda <batmoo@gmail.com>
2014-07-17 16:04:55 -0400

    Fix require path for renamed Exception class

--- src/wpcom.php
+++ src/wpcom.php
@@ -4,7 +4,7 @@
 $base_dir =  dirname( __FILE__ );
 require_once( $base_dir . '/class-wp-rest-client.php' );
 require_once( $base_dir . '/class-wp-rest-request.php' );
-require_once( $base_dir . '/class-wpcom-rest-exception.php' );
+require_once( $base_dir . '/class-wp-rest-exception.php' );
 require_once( $base_dir . '/class-wp-rest-transport.php' );
 require_once( $base_dir . '/class-wp-rest-transport-curl.php' );
 require_once( $base_dir . '/class-wp-rest-object.php' );
